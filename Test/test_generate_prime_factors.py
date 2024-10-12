6a2a2372508bd3b334e798f8b716893ce9916de2
brianb85 <brian.blaker@outlook.com>
2024-10-12 18:26:00 -0400

    Assignment 3
    Step 1

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,7 @@
+import pytest
+import prime
 
+
+def test_datatype():
+    with pytest.raises(ValueError):
+        prime.generate_prime_factors("Testing")
--- prime.py
+++ prime.py
@@ -1,3 +1,9 @@
 """
 prime.py -- Write the application code here
 """
+
+
+def generate_prime_factors(value):
+    int(value)
+    return
+
