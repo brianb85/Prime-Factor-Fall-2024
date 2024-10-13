b38b46fc1a9864e1735d1093ced55833b7835773
brianb85 <brian.blaker@outlook.com>
2024-10-12 20:30:29 -0400

    Assignment 3
    Step 8

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -6,20 +6,33 @@ def test_step1_datatype():
     with pytest.raises(ValueError):
         prime.generate_prime_factors("Testing")
 
+
 def test_step2_one():
     assert prime.generate_prime_factors(1) == []
+
 
 def test_step3_two():
     assert prime.generate_prime_factors(2) == [2]
 
+
 def test_step4_three():
     assert prime.generate_prime_factors(3) == [3]
+
 
 def test_step5_four():
     assert prime.generate_prime_factors(4) == [2, 2]
 
+
 def test_step6_six():
     assert prime.generate_prime_factors(6) == [2, 3]
 
+
 def test_step7_eight():
     assert prime.generate_prime_factors(8) == [2, 2, 2]
+
+
+def test_step8_nine():
+    assert prime.generate_prime_factors(9) == [3, 3]
+
+def test_step9_850():
+    assert prime.generate_prime_factors(850) == [2, 5, 5, 17]
--- prime.py
+++ prime.py
@@ -6,11 +6,11 @@ prime.py -- Write the application code here
 def generate_prime_factors(value):
     int(value)
     list1 = []
-    list2 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-    while(value >= 2):
-        for num in list2:
-            if (value % num == 0):
-                list1.append(num)
-                value = value / num
-
+    num = 2
+    while value >= 2:
+        if value % num == 0:
+            list1.append(num)
+            value = value / num
+        else:
+            num = num + 1
     return list1
